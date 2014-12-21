1b89c15f5edc1f5e56a078e216366ea0785086c6
dvberkel <daan.v.berkel.1980@gmail.com>
2014-12-21 10:43:53 +0100

    start with a ball of radius 10

--- js/table.js
+++ js/table.js
@@ -1,5 +1,5 @@
 var table = (function(fps, billiard, undefined){
-	var table = new billiard.Table({ width: 640, height: 480 });
-	table.addBall({ x: 0, y: 1, vx: 3, vy: 2 });
+	var table = new billiard.Table({ width: 320, height: 240 });
+	table.addBall({ x: 0, y: 1, vx: 3, vy: 2, r: 10 });
 	return table;
 })(fps, billiard);
