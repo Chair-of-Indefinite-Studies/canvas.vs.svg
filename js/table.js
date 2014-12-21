666ede0757543c756dd2feaab0fda2801eb1e504
dvberkel <daan.v.berkel.1980@gmail.com>
2014-12-21 13:24:32 +0100

    allow for a variable number of balls

--- js/table.js
+++ js/table.js
@@ -1,5 +1,19 @@
 var table = (function(fps, billiard, undefined){
-	var table = new billiard.Table({ width: 320, height: 240 });
-	table.addBall({ x: 0, y: 1, vx: 3, vy: 2, r: 10 });
+	function range(l, h) {
+		var result = [];
+		while (h > l) { result[--h] = h; }
+		return result;
+	}
+
+	var options = { width: 320, height: 240, collisions: false };
+	var table = new billiard.Table(options);
+
+	(range(0, 1)).forEach(function(_, index){
+		var x = Math.floor(2 * options.width * (Math.random() - 1/2));
+		var y = Math.floor(2 * options.height * (Math.random() - 1/2));
+		var vx = Math.floor(10 * (Math.random() - 1/2));
+		var vy = Math.floor(6 * (Math.random() - 1/2));
+		table.addBall({ x: x, y: y, vx: vx, vy: vy, r: 10 });
+	});
 	return table;
 })(fps, billiard);
